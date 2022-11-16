f4fd060dc3fdbf9b33536d425b312011a7c2fa25
Jayvis Wisely <jayviswise@gmail.com>
2022-11-16 18:52:44 +0700

    Revised Description

--- components/Description.jsx
+++ components/Description.jsx
@@ -4,7 +4,7 @@ export default function Description() {
     return (
         <div className="description">
             <h1>About</h1>
-            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p>I am a high school student, who is enthusiast for the world of Frontend Development. I am proficient in HTML, CSS, and JS, and is now learning to develop with React. I am highly interested in MERN Stack. I try to keep up with best practices, and am always looking for new things to learn.</p>
             <h1>Who I am</h1>
             <p>Frontend Developer. Student. Freelancer. Video Editor. Pixel Artist. Indie Pop Enjoyer. Fingerstyle Guitarist. Sleep-deprived person.</p>
         </div>
