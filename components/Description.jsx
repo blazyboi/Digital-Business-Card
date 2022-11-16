4ad407d1817e4fed7a2423288627034bf844161e
Jayvis Wisely <jayviswise@gmail.com>
2022-11-16 18:31:15 +0700

    Fixed Texts

--- components/Description.jsx
+++ components/Description.jsx
@@ -5,7 +5,7 @@ export default function Description() {
         <div className="description">
             <h1>About</h1>
             <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-            <h1>I'm a :</h1>
+            <h1>Who I am</h1>
             <p>Frontend Developer. Student. Freelancer. Video Editor. Pixel Artist. Indie Pop Enjoyer. Fingerstyle Guitarist. Sleep-deprived person.</p>
         </div>
     )
