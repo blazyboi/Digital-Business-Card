68d43ddad1592c91547274baa3c5fdf71e47528c
Jayvis Wisely <jayviswise@gmail.com>
2022-11-19 21:33:05 +0700

    Added project images

--- components/Description.jsx
+++ components/Description.jsx
@@ -7,7 +7,8 @@ export default function Description() {
             <p>I am a high school student, who is enthusiast for the world of Frontend Development. I am proficient in HTML, CSS, and JS, and is now learning to develop with React. I am highly interested in MERN Stack. I try to keep up with best practices, and am always looking for new things to learn.</p>
             <h1>Who I am</h1>
             <p>Frontend Developer. Student. Freelancer. Video Editor. Pixel Artist. Indie Pop Enjoyer. Fingerstyle Guitarist. Sleep-deprived person.</p>
-            <h1>My Projects :</h1>
+            <h1>Some of My Projects</h1>
+            <p>Below are some of my created projects :</p>
         </div>
     )
 }
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,9 @@
 import React from "react"
 import project1 from "../images/projects/portfolio.jpg"
+import project2 from "../images/projects/ssec.jpg"
+import project3 from "../images/projects/reactfacts.jpg"
+import project4 from "../images/projects/card.jpg"
+import project5 from "../images/projects/aeimnestus.jpg"
 
 export default function Projects() {
     return (
@@ -11,15 +15,19 @@ export default function Projects() {
                 </li>
                 <li>
                     <h1 className="projects--title">SSEC Mini Arcade</h1>
+                    <img className="projects--img" src={project2}></img>
                 </li>
                 <li>
                     <h1 className="projects--title">Web Technologies Facts</h1>
+                    <img className="projects--img" src={project3}></img>
                 </li>
                 <li>
                     <h1 className="projects--title">Digital Business Card</h1>
+                    <img className="projects--img" src={project4}></img>
                 </li>
                 <li>
                     <h1 className="projects--title">Aeimnestus</h1>
+                    <img className="projects--img" src={project5}></img>
                 </li>
             </ul>
         </div>
