04b60267ba3d999ee88d86893d0086d9d39297e6
Jayvis Wisely <jayviswise@gmail.com>
2022-11-19 07:29:46 +0700

    Fixed Projects Return

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,23 +1,25 @@
 import React from "react"
 
 export default function Projects() {
-    <div className="projects">
-        <ul>
-            <li>
-                <h1>1. Portfolio Website</h1>
-            </li>
-            <li>
-                <h1>2. SSEC Mini Arcade</h1>
-            </li>
-            <li>
-                <h1>3. Web Technologies Facts</h1>
-            </li>
-            <li>
-                <h1>4. Digital Business Card</h1>
-            </li>
-            <li>
-                <h1>5. Aeimnestus</h1>
-            </li>
-        </ul>
-    </div>
+    return (
+        <div className="projects">
+            <ul>
+                <li>
+                    <h1>1. Portfolio Website</h1>
+                </li>
+                <li>
+                    <h1>2. SSEC Mini Arcade</h1>
+                </li>
+                <li>
+                    <h1>3. Web Technologies Facts</h1>
+                </li>
+                <li>
+                    <h1>4. Digital Business Card</h1>
+                </li>
+                <li>
+                    <h1>5. Aeimnestus</h1>
+                </li>
+            </ul>
+        </div>
+    )
 }
