0f94dfc2fb91e93f9796b046b1fc54eab9ce12ba
Jayvis Wisely <jayviswise@gmail.com>
2022-11-19 10:58:11 +0700

    Image Test

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import project1 from "../images/projects/portfolio.jpg"
 
 export default function Projects() {
     return (
@@ -6,6 +7,7 @@ export default function Projects() {
             <ul className="projects--list">
                 <li>
                     <h1 className="projects--title">Portfolio Website</h1>
+                    <img className="projects--img" src={project1}></img>
                 </li>
                 <li>
                     <h1 className="projects--title">SSEC Mini Arcade</h1>
